bfc6f0c5a99512be2a7522bfe5babf769b75ad85
lucasarago <lbohorquez@Aragoconsulting.com>
2021-03-21 19:10:15 -0500

    Procedimiento inserción

--- dist/js/script.js
+++ dist/js/script.js
@@ -36,3 +36,20 @@ function misterio() {
     document.getElementById("preguntaDos").innerHTML += 
       "Valor x: " + x + " Valor y: " + y + " Valor z: " + z;
   }
+
+  function procedimientoInsercion(){
+    let A = document.getElementById("arregloInsercion").value.split(",");
+    let n = A.length;
+    let x; 
+    let j;
+    for (let i = 1; i < n; i++){
+      x = A[i];
+      j = i - 1;
+      while(x < A[j] && j > 0){
+        A[j + 1] = A[j];
+        j = j - 1;
+      }
+      A[j + 1] = x;
+    }
+    document.getElementById("resultadoInsercion").innerText = A;
+  }
